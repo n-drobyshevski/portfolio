48b8b6a590f6ddf7c944c3a2efd9e6d5cabaeba7
Nick <n.drobyshevski@pm.me>
2022-11-11 13:32:15 +0100

    fix: layout component grid

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,7 +8,7 @@ type LayoutProps = {
 };
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="container relative flex h-full flex-col">
+    <div className="container grid h-full grid-cols-9 grid-rows-[100px_1fr_50px]">
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -18,11 +18,21 @@ const Layout = ({ children }: LayoutProps) => {
         <meta name="og:title" content="Portfolio" />
       </Head>
       <Header />
-      {/* <main className="my-12 grid h-max grow grid-cols-7 gap-6">
+      <main className="col-start-2 col-end-9 row-start-2 row-end-3 mt-6 gap-6 border border-white">
         {children}
-      </main> */}
-      <ScrollIndicator top={false} bottom={true} />
-      <Sidebar currentSectionName="about" />
+      </main>
+      <div
+        className="col-start-10 col-end-9 row-start-2 row-end-4"
+        id="sidebar"
+      >
+        <Sidebar currentSectionName="about" />
+      </div>
+      <div
+        className="relative col-start-1 col-end-2 row-start-2 row-end-4"
+        id="scrollIndicator"
+      >
+        <ScrollIndicator top={false} bottom={true} />
+      </div>
     </div>
   );
 };
