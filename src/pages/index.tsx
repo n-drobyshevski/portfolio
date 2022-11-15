9191f4ba7e8ab00b43ae20a7da61a431ca54a422
Nick <n.drobyshevski@pm.me>
2022-11-15 13:33:33 +0100

    feat: Skills Section Component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import Layout from '@/components/Layout';
 import Sidebar from '@/components/Sidebar';
 import HomeSection from '@/components/sections/HomeSection';
 import AboutSection from '@/components/sections/AboutSection';
+import SkillsSection from '@/components/sections/SkillsSection';
 const Home: NextPage = () => {
   return (
     <div className="container h-screen w-screen bg-neutral-900 px-56 pb-8">
       <Layout>
         {/* <HomeSection /> */}
-        <AboutSection />
+        {/* <AboutSection /> */}
+        <SkillsSection />
       </Layout>
     </div>
   );
