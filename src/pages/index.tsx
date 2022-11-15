6a0dcf7ede0f82664d5d3319b74637247480a46d
Nick <n.drobyshevski@pm.me>
2022-11-15 22:24:36 +0100

    feat: Projects Section component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,15 @@ import Sidebar from '@/components/Sidebar';
 import HomeSection from '@/components/sections/HomeSection';
 import AboutSection from '@/components/sections/AboutSection';
 import SkillsSection from '@/components/sections/SkillsSection';
+import ProjectSection from '@/components/sections/ProjectsSection';
 const Home: NextPage = () => {
   return (
     <div className="container h-screen w-screen bg-neutral-900 px-56 pb-8">
       <Layout>
         {/* <HomeSection /> */}
         {/* <AboutSection /> */}
-        <SkillsSection />
+        {/* <SkillsSection /> */}
+        <ProjectSection />
       </Layout>
     </div>
   );
